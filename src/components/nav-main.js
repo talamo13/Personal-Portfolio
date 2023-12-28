9ed3a31a66b2d8d6d3712b3163dd56f0d88d50b8
Tyler <tyleralamo15@gmail.com>
2023-12-28 10:47:07 -0800

    added the start of the navbar

--- src/components/nav-main.js
+++ src/components/nav-main.js
@@ -1,8 +1,29 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 function NavMain() {
   return (
-    <div>
-      <h3>This is going to be the NavBar!</h3>
-    </div>
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand as={Link} to={"/"}>
+            Tyler Alamo-Covert
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to={"/about"}>
+              About
+            </Nav.Link>
+            <Nav.Link as={Link} to={"/projects"}>
+              Projects
+            </Nav.Link>
+            <Nav.Link as={Link} to={"/contact"}>
+              Contact
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   );
 }
 

975d4dcebf142efd4cd35f0f1ad5636b415d98d2
Tyler <tyleralamo15@gmail.com>
2024-01-10 17:58:49 -0800

    making progress on landing and contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,35 @@
+import PageContainer from "../components/page-container";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 function Contact() {
   return (
-    <div>
-      <h1>Contact Page</h1>
-    </div>
+    <PageContainer>
+      <div
+        style={{
+          backgroundColor: "grey",
+          display: "flex",
+          width: "75%",
+          borderRadius: "10px",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "auto",
+        }}
+      >
+        {/* First Half Of The Page*/}
+        <div style={{ padding: "25px" }}>
+          <Container>
+            <Row>Contact Me!</Row>
+            <Row>tyleralamo15@gmail.com</Row>
+          </Container>
+        </div>
+        {/* First Half Of The Page*/}
+        <div>
+          <h2>Contact Form Will Be Here</h2>
+        </div>
+      </div>
+    </PageContainer>
   );
 }
 
